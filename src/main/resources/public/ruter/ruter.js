cb31f636dfeec9a502186b234affb51432c7a4b1
Jøran Vagnby Lillesand <lillesand@gmail.com>
2017-07-06 21:26:28 +0200

    En høyst basic layout
    Ikke mye pent. Men bedre enn det var!

--- src/main/resources/public/ruter/ruter.js
+++ src/main/resources/public/ruter/ruter.js
@@ -1,21 +1,37 @@
-const departuresContainer = document.querySelector('#departures');
+(function() {
 
-fetch('/ruter', {
-    headers: {
-        'Accept': 'application/json'
-    }
-}).then(function(response) {
-    return response.json();
-}).then(function(json) {
+    const departuresContainer = document.querySelector('#departures');
 
-    let html = `<div class="departures">`;
-    json.departures.forEach(function(departure) {
-        html += `<div class="departure">
+    const departureConfig = {
+        times: {
+            tooLate: 3,
+            lower: 5,
+            upper: 30
+        },
+        max: 5
+    };
+
+    fetch('/ruter', {
+        headers: {
+            'Accept': 'application/json'
+        }
+    }).then(function(response) {
+        return response.json();
+    }).then(function(json) {
+
+        let html = `<div class="departures">`;
+        html += json.departures.map(departureToHtml).join('');
+        html += `</div>`;
+
+        departuresContainer.innerHTML = html;
+    });
+
+    function departureToHtml(departure) {
+        return `<div class="departure">
     <span class="time">${departure.waitingTimeInMinutes}</span>
     <span class="line">${departure.destinationName} (${departure.lineName})</span>
-</div>`
-    });
-    html += `</div>`;
+</div>`;
+    }
 
-    departuresContainer.innerHTML = html;
-});
+})();
+
