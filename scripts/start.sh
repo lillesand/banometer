fc69fca22f6eec4f7ea2110fcff26dd2900320a3
Jøran Vagnby Lillesand <lillesand@gmail.com>
2018-03-28 09:12:18 +0200

    Don't write to stdout from bash script
    It looks like writing to stdout from bash directly crashes, probably because there is no stdout (?!)

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 
 DELAY_SECONDS=$1
+sleep $DELAY_SECONDS;
 
-echo "Sleeping $DELAY_SECONDS before starting"
-sleep ${DELAY_SECONDS};
-
-echo "Pulling latest changes from git"
 git pull
-
-echo "Building"
 ./gradlew build -x test
 
-echo "Launching application!"
 java -jar build/libs/banometer-1.0.jar
