d48b43a53a20670ee963a01c790bc363d34fd4ea
Jøran Vagnby Lillesand <lillesand@gmail.com>
2020-05-23 16:36:38 +0200

    Added routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import './App.css';
+import { Route, Switch } from 'react-router';
 import WineView from './wines/WineView';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
-  return (
-    <WineView/>
-  );
+  return <>
+    <div>
+      Flibbediflopp
+    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/wines">
+          <WineView/>
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  </>;
 }
 
 export default App;
