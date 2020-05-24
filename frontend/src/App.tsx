562ebf39cf2b636716130553a009f0f07079683e
Jøran Vagnby Lillesand <lillesand@gmail.com>
2020-05-24 14:37:03 +0200

    Added some super simple styled navigation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import './App.css';
 import { Route, Switch } from 'react-router';
 import { NetatmoView } from './weather/NetatmoView';
 import { BrowserRouter, Link } from 'react-router-dom';
 import { WineView } from './wines/WineView';
+import './App.scss';
 
 function App() {
   return <>
     <BrowserRouter>
-      <nav>
+      <nav className="main-navigation">
         <ul>
           <li><Link to="/wines">Vin</Link></li>
           <li><Link to="/weather">Vær</Link></li>
