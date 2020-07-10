97c6b119bbfac3f1ee8e684f3c1269fd2bf95b1f
Jøran Vagnby Lillesand <lillesand@gmail.com>
2020-07-10 12:07:21 +0200

    Made main contents scrollable
    … thanks to my super awesome DRAGSCROLL implementation. It's… okay.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,24 +4,31 @@ import { NetatmoView } from './weather/NetatmoView';
 import { BrowserRouter} from 'react-router-dom';
 import { WineView } from './wines/WineView';
 import { Menu } from './menu/Menu';
-import './App.scss';
 import { ForecastView } from './forecast/ForecastView';
+import { Dragscroll } from './utils/dragscroll/Dragscroll';
+import './App.scss';
 
 function App() {
   return <>
     <BrowserRouter>
-      <Switch>
-        <Route path="/wines">
-          <WineView />
-        </Route>
-        <Route path="/temperature">
-          <NetatmoView />
-        </Route>
-        <Route path="/forecast">
-          <ForecastView />
-        </Route>
-      </Switch>
-      <Menu />
+      <div className="app">
+        <section className="main">
+          <Switch>
+            <Route path="/wines">
+              <WineView />
+            </Route>
+            <Route path="/temperature">
+              <NetatmoView />
+            </Route>
+            <Route path="/forecast">
+              <Dragscroll>
+                <ForecastView />
+              </Dragscroll>
+            </Route>
+          </Switch>
+        </section>
+        <Menu />
+      </div>
     </BrowserRouter>
   </>;
 }
