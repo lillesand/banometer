434e1fbadd41cae5fd0545bae9a3e8c0b5262738
Jøran Vagnby Lillesand <lillesand@gmail.com>
2020-07-11 11:02:51 +0200

    Enable CORS for backend

--- src/main/kotlin/banometer/Application.kt
+++ src/main/kotlin/banometer/Application.kt
@@ -5,10 +5,16 @@ import netatmo.NetatmoController
 import odeon.CinemaController
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 import org.springframework.scheduling.annotation.EnableScheduling
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 import weather.MetController
 import weather.YrProxyController
 import winesync.WineController
+
 
 @SpringBootApplication(scanBasePackageClasses = [
     EnturController::class,
@@ -20,6 +26,8 @@ import winesync.WineController
     WineController::class
 ])
 @EnableScheduling
+@Configuration
+@EnableWebMvc
 open class Application {
 
     companion object {
@@ -27,5 +35,16 @@ open class Application {
             SpringApplication.run(Application::class.java, *args)
         }
     }
+
+    @Bean
+    open fun corsConfigurer(): WebMvcConfigurer {
+        println("Configurer")
+        return object : WebMvcConfigurer {
+            override fun addCorsMappings(registry: CorsRegistry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:3000", "http://localhost:80")
+            }
+        }
+    }
+
 }
 
