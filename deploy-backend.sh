8b958cb8d85666962653b86fa22267e3f1c6e5da
Jøran Vagnby Lillesand <lillesand@gmail.com>
2020-10-29 11:06:14 +0100

    Minor deploy script changes
    Mostly changed IP for the new raspi

--- deploy-backend.sh
+++ deploy-backend.sh
@@ -1,8 +1,8 @@
 ./gradlew bootJar
-scp build/libs/banometer-kotlin.jar pi@192.168.86.22:~/banometer/
+scp build/libs/banometer-kotlin.jar pi@192.168.86.184:~/banometer/
 
 # Use if service definition is new or changed:
-#scp banometer.service pi@192.168.86.22:~/banometer/
-#ssh pi@192.168.86.22 "sudo systemctl daemon-reload"
+#scp banometer.service pi@192.168.86.184:~/banometer/
+#ssh pi@192.168.86.184 "sudo systemctl daemon-reload"
 
-ssh pi@192.168.86.22 "sudo systemctl restart banometer"
+ssh pi@192.168.86.184 "sudo systemctl restart banometer"
