671fc2b2af6a87d8c1e6a6b3849e093328ede80c
Jøran Vagnby Lillesand <lillesand@gmail.com>
2020-10-29 16:31:03 +0100

    Now: nginx for frontend hosting
    Not having proper single page webapp support what getting real old real quick. Now we do!

--- deploy-frontend.sh
+++ deploy-frontend.sh
@@ -2,8 +2,4 @@ cd frontend || exit && npm run build && cd ..
 ssh pi@192.168.86.184 "rm -r ~/banometer/frontend/**"
 scp -r frontend/build/** pi@192.168.86.184:~/banometer/frontend/
 
-# Use if service definition is new or changed:
-#scp banometer-frontend.service pi@192.168.86.184:~/banometer/banometer-frontend.service
-#ssh pi@192.168.86.184 "sudo systemctl daemon-reload"
-
-ssh pi@192.168.86.184 "sudo systemctl restart banometer-frontend"
+ssh pi@192.168.86.184 "sudo systemctl restart nginx"
