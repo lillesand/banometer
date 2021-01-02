cc6d8cb39691fcd848b6f31579c2c487dc17ff07
Jøran Vagnby Lillesand <lillesand@gmail.com>
2021-01-02 16:41:37 +0100

    Extracted firebase photo api stuff to a separate effect
    ftw

--- frontend/src/photos/PhotosView.tsx
+++ frontend/src/photos/PhotosView.tsx
@@ -1,23 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import styles from './PhotosView.module.scss';
-import database from '../firebase-storage/config';
 import { LinkItem } from '../menu/LinkItem';
 import { TopBarNavigation } from '../topBarNavigation/TopBarNavigation';
 import { makeRequest } from '../http/makeRequest';
-
-interface PhotoResponse {
-  requested_at: string;
-  status: 'REQUESTED' | 'PROCESSING' | 'DONE';
-  url?: string;
-}
-
-interface ApiResponse {
-  data?: {
-    result: PhotoResponse[];
-  };
-  error?: string;
-  loading: boolean;
-}
+import { useFirebasePhotos } from './useFirebasePhotos';
 
 const requestPhoto = async () => {
   try {
@@ -29,55 +15,24 @@ const requestPhoto = async () => {
 }
 
 export const PhotosView = () => {
+  const [photos, loading, error] = useFirebasePhotos(`/test/banometer/photos/jorbu/`);
 
-  const [existingPhotosResponse, setExistingPhotosResponse] = useState<ApiResponse>({ loading: true });
-  const [newPhotos, setNewPhotos] = useState<PhotoResponse[]>([]);
-
-  const fetchPhotos = async () => {
-    const res = await database.ref(`/test/banometer/photos/jorbu/`).limitToLast(10).orderByKey().once('value');
-    const data = await res.val() ? Object.values(res.val()) : [];
-    setExistingPhotosResponse({
-      loading: false,
-      data: {
-        result: data as PhotoResponse[],
-      },
-    });
-  }
-
-  useEffect(() => {
-    fetchPhotos().catch((error) => {
-      console.error(error);
-      setExistingPhotosResponse({ loading: false, error: error.toString()})
-    });
-  }, []);
-
-  useEffect(() => {
-    database.ref(`/test/banometer/photos/jorbu/`)
-      .on('child_changed', (res) => {
-        const newPhoto = res.val() as PhotoResponse;
-        if (newPhoto.status === 'DONE') {
-          setNewPhotos([newPhoto, ...newPhotos]);
-        }
-      })
-  }, [newPhotos]);
-
-  if (!existingPhotosResponse.data) {
+  if (loading) {
     return <div>laddar…</div>;
   }
 
-  if (existingPhotosResponse.error) {
-    return <div>Feilet: {existingPhotosResponse.error}</div>
+  if (error) {
+    return <div>Feilet: {error}</div>
   }
 
-  const sortedExistingPhotos = existingPhotosResponse.data.result.sort((a, b) => b.requested_at.localeCompare(a.requested_at));
-  const allPhotos = newPhotos.concat(sortedExistingPhotos);
+  const sortedPhotos = photos.sort((a, b) => b.requested_at.localeCompare(a.requested_at));
 
   return <div className={styles.photosView}>
     <TopBarNavigation title={{capitalized: "Bilder fra hytta"}}>
       <LinkItem onClick={requestPhoto} emoji="📸" text="Ta bilde" />
     </TopBarNavigation>
     {
-      allPhotos.map(photo =>
+      sortedPhotos.map(photo =>
         <div className={styles.photoBlock} key={`photo-${photo.requested_at}`}>
           {
             photo.url
---
+++ frontend/src/photos/useFirebasePhotos.ts
@@ -0,0 +1,41 @@
+import { useCallback, useEffect, useState } from 'react';
+import database from '../firebase-storage/config';
+
+export interface PhotoResponse {
+  requested_at: string;
+  status: 'REQUESTED' | 'PROCESSING' | 'DONE';
+  url?: string;
+}
+
+export const useFirebasePhotos = (firebasePath: string) : [PhotoResponse[], boolean, Error?] => {
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<Error>();
+  const [photos, setPhotos] = useState<PhotoResponse[]>([]);
+
+  const fetchPhotos = useCallback(async () => {
+    const res = await database.ref(firebasePath).limitToLast(10).orderByKey().once('value');
+    const data = await res.val() ? Object.values(res.val()) : [];
+    setLoading(false);
+    setPhotos(data as PhotoResponse[]);
+  }, [firebasePath]);
+
+  useEffect(() => {
+    fetchPhotos().catch((error) => {
+      console.error(error);
+      setLoading(false);
+      setError(error);
+    });
+  }, [fetchPhotos]);
+
+  useEffect(() => {
+    database.ref(firebasePath)
+      .on('child_changed', (res) => {
+        const newPhoto = res.val() as PhotoResponse;
+        if (newPhoto.status === 'DONE') {
+          setPhotos([newPhoto, ...photos]);
+        }
+      })
+  }, [photos, firebasePath]);
+
+  return [photos, loading, error];
+}
